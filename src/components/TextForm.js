e9ec9854a16373653a32300afb1b71cbfc4b69f0
ayush-9453 <agrawal9453@gmail.com>
2023-08-15 21:53:05 +0530

    adding summary

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,34 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText)
   }
+  const handleLowClick =()=>{
+     let newText = text.toLowerCase();
+     setText(newText)
+  }
 
   const handleOnChange = (event)=>{
     setText(event.target.value);
   }
   
-  const [text, setText] = useState("Enter Text here");
+  const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className="container">
     <h1>{props.heading}</h1>
     <div className="mb-3">
     <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
+    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
+
     </div>
+    <div className="container my-4">
+      <h3>Your text summary</h3>
+      <p >{text.split(" ").length} words, {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read the text</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }

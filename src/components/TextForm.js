2f787888ac01d11d0d8dec4aed6392ae2bd6e2ef
ayush-9453 <agrawal9453@gmail.com>
2023-08-16 23:50:20 +0530

    commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,10 +36,10 @@ export default function TextForm(props) {
   const [text, setText] = useState('');
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode ==="dark"?"white":"#3E454C" }}>
     <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
+    <textarea className="form-control" value={text} style={{backgroundColor: props.mode ==="dark"?"grey":"white",color: props.mode ==="dark"?"white":"#3E454C" }} id="myBox" onChange={handleOnChange} rows="8"></textarea>
     </div>
     <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
     <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -48,12 +48,12 @@ export default function TextForm(props) {
     <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
 
     </div>
-    <div className="container my-4">
+    <div className="container my-4" style={{color: props.mode ==="dark"?"white":"#3E454C" }}>
       <h3>Your text summary</h3>
       <p >{text.split(" ").length} words, {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes to read the text</p>
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter the text to previ"}</p>
     </div>
     </>
   )

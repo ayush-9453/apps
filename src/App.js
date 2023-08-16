cb98704315e0cb4db6340e38bf172214490a678a
ayush-9453 <agrawal9453@gmail.com>
2023-08-16 23:19:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [mode, setMode]= useState("light");
+  const [mode, setMode]= useState('light');
   const toggleMode =()=>{
-    if(mode =='dark'){
-      setMode('light')
-    }else{
+    if(mode =='light'){
       setMode('dark')
+      document.body.style.backgroundColor ='grey';
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor ='white';
     }
   }
   return (

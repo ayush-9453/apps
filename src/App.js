324aa14ceaaeb05f8a45781505856c4cf43e0fc8
ayush-9453 <agrawal9453@gmail.com>
2023-08-16 09:37:06 +0530

    words

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
   <>
-<NavBar title="Mr.Panner" aboutText="About Us"/>
+<NavBar title="Mr.Word" aboutText="About Us"/>
 <div className="container my-3">
 <TextForm heading="Enter the text to the Box"/> 
 {/*<About/>*/}

a885baaa4f0c4a3446be699e823a8895f53567aa
ayush-9453 <agrawal9453@gmail.com>
2023-08-17 19:56:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import TextForm from './components/TextForm';
  //import About from './components/About';
 
 import './App.css';
-import { useState } from 'react';
+import React ,{ useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode]= useState('light');
+  const [alert, setAlert]= useState("hello","null");
+
+  const showAlert =(message, type)=>{
+     setAlert({
+      msg: message,
+      type:type
+     })
+  }
+
   const toggleMode =()=>{
-    if(mode =='light'){
+    if(mode ==='light'){
       setMode('dark')
       document.body.style.backgroundColor ='#3E454C';
+      showAlert("Dark mode has been enabled","success")
     }else{
       setMode('light')
       document.body.style.backgroundColor ='white';
+      showAlert("Light mode has been enabled","success")
     }
   }
   return (
   <>
 <NavBar title="Mr.Word" mode={mode} toggleMode={toggleMode}/>
-<div className="container my-3">
+ <Alert alert  ={alert} />
+ <div className="container my-3">
 <TextForm heading="Enter the text to the Box" mode={mode}/> 
 {/*<About/>*/}
 </div>

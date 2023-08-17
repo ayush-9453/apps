8e93ec0b065c20a7e50e3d0181db17add23e6237
ayush-9453 <agrawal9453@gmail.com>
2023-08-18 00:06:09 +0530

    c

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,7 +8,7 @@ export default function Alert(props) {
   return (
       props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
       <strong>{captitalized(props.alert.type)}</strong> : {props.alert.msg}
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  
       </div>
   )
 }

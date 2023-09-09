1b5da8cdd9073456242b45456c603862f09f23a5
ayush-9453 <98285114+ayush-9453@users.noreply.github.com>
2023-09-09 19:21:08 +0530

    c

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,1 +1,18 @@
-print("Hello World")
+print("Hello World") # print statement
+
+
+#Type of variable
+b= true #boolean 
+c= "as"  #string 
+d = None
+a=1 #variable 
+print(a) #print variable a 
+print(type(a)) # prints the type of variable 
+list1 =[8, 7 , ["apple ", "banana"]] #list
+print(list1)
+  #dictionary / map
+dict1={            
+ "name ": "as",
+  "class" : 34, 
+  "can" : True
+}

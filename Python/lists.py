f2764e993980f4ca410ffaca6916f15343cf3ba7
ayush9453 <agrawal9453@gmail.com>
2023-09-18 22:16:54 +0530

    18/9

---
+++ Python/lists.py
@@ -0,0 +1,20 @@
+# fruits is a list 
+fruits =['oranges', 'apple','bananas','kiwi','apple']
+
+# This count the number of occurce of a string or a value in a list
+print(fruits.count('apple'))
+
+#This tells the index of the value in a string
+print(fruits.index('bananas'))
+#Now this finds the value starting at 2
+print(fruits.index('apple',2))
+
+#to reverse a list
+print(fruits.reverse())
+
+# to append a value in a list
+print(fruits.append('kiwi'))
+
+#t0 sort a list 
+print(fruits.sort())
+

f47a1cdbd6d8d2c3a680100e853dd724477b692d
ayush9453 <agrawal9453@gmail.com>
2023-09-19 10:31:39 +0530

    19/9

--- Python/lists.py
+++ Python/lists.py
@@ -36,3 +36,31 @@ for x in range(10):
     squares.append(x**2)
 
 print(squares)
+
+combs =[]
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x!=y:
+            combs.append((x,y))
+
+print(combs)
+
+# # for transpose in matrix
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# transposed=[]
+# for i in range(4):
+#     transposed_row =[]
+#     for row in matrix:
+#        transposed_row.append(row[i])
+#     transposed.append(transposed_row)
+
+# print(transposed)
+
+# for a delete operations
+a = [1 ,-1,3 ,35, 34 ,36,353]
+del a[0]
+print(a)
+del a[2:4] # for in range del opertaions
+print(a)
+del a[:]
+print(a) # to empty the list

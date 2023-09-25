b87b186ad0f6e51bc115b43b75078d91f8981c95
ayush9453 <agrawal9453@gmail.com>
2023-09-25 21:56:45 +0530

    sets

---
+++ set.py
@@ -0,0 +1,12 @@
+# set in python removes any repeated data
+# it is a well define data set and unordered data
+info = {
+    "carlos",15, False,3.55
+}
+print(info)
+
+name = set()
+print(type(name))
+
+for value in info:
+    print(value)

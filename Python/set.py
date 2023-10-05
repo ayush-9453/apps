9a126af5d6f6e0b8189b48d7320e02b0b0d8917b
ayush9453 <agrawal9453@gmail.com>
2023-10-05 19:51:07 +0530

    commit

--- Python/set.py
+++ Python/set.py
@@ -10,3 +10,23 @@ print(type(name))
 
 for value in info:
     print(value)
+
+# set joining
+s1 = {1,2,3,5}
+s2 = {2,4,7,9}
+print(s1.union(s2)) 
+s1.update(s2)
+print(s1,s2)
+
+# set intersection
+print(s1.intersection(s2))
+s1.intersection_update(s2)
+print(s1)
+
+# symetric difference
+s1.symmetric_difference(s2)
+print(s1)
+
+# difference
+s1.difference(s2)
+print(s1)

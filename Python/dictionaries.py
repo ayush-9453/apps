52cdc85dbdd73af1e956db7c68760b93e2652a9a
ayush9453 <agrawal9453@gmail.com>
2023-10-08 00:07:41 +0530

    commit

--- Python/dictionaries.py
+++ Python/dictionaries.py
@@ -9,7 +9,22 @@ print(dic.keys())
 for key in dic.keys():
     print(f"The value corresponding to the key {key} is {dic[key]}")
 
+# Dictionaries methods
+
 ep ={12:  24 , 13:69 ,14:69 ,15:79}
 ep2 ={20:50 ,21:46  }
 ep.update(ep2)
 print(ep)
+
+# to clear an item from the dictionary
+ep.clear()
+print(ep)
+
+# to pop an item from the dictionary
+ep.pop(12)
+print(ep)
+
+# to delete a dictionary
+del ep
+del ep[15]
+
--- Python/quiz.py
+++ Python/quiz.py
@@ -1,11 +1,24 @@
 questions =[
-    '5+2','3*4','3-2','55+11'
+    ['5+2=',7,8,9,4,1],
+    ['3*4=',12,31,45,9,1 ],
+    ['33-2=',31,36,43,51,1],
+    ['55+11=',66,72,45,78,1]
 ]
-ques =3
-answer =['7','12','1','66']
 
-for i in questions:
-    print(i)
-    a =  int(input("Enter the answer:"))
-
-print(answer)
+level = [100, 200, 4000,23455,]
+win = 0
+i =0
+for i in range(0,len(questions)):
+    question =questions[i]
+    print(f"Question for Rs.{level[i]}")
+    print(f"Q{i+1}. {question[0]}")
+    print(f"a.{question[1]}    b.{question[2]}")
+    print(f"c.{question[3]}    b.{question[4]}")
+    reply =int(input("Enter the answer :"))
+    if (reply == question[-1]):
+        print(f"Correct answer , you won {level[i]}")
+        win =win +level[i]
+        print(win)
+    else :
+        print("Worng answer, you lost")
+        break

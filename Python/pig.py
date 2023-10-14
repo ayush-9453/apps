4336c31cfbebd5707562ca1df63488f4c7800ae7
ayush9453 <agrawal9453@gmail.com>
2023-10-14 21:32:41 +0530

    commit

---
+++ Python/pig.py
@@ -0,0 +1,41 @@
+import random
+
+def roll():
+    min_value = 1 
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+    return roll
+
+while(True):
+    players = input("Enter the number of players (2-4):")
+    if players.isdigit():
+        players=int(players)
+        if 1<=players <=4:
+            print("Must be between 1-4 players.")
+            break
+    else:
+        print("Invalid, try again.")
+
+max_score=50 
+player_score = [ 0 for _ in range(players)]
+
+while max(player_score)< max_score:
+    for player_idk in range(players):
+       current_score = 0
+
+    while True:
+        should_roll = input("would you like to roll (y)?")
+        if should_roll.lower() !="y":
+         break
+        value = roll()
+        if value ==1:
+          print("You rolled 1! Trun done!")
+          current_score=0
+          break
+        else:
+           current_score += value
+           print("You rolled a:" ,value)
+        print("Your score is:", current_score)
+
+    player_score[player_idk] += current_score
+    print("Your total score is :",player_score[player_idk])
